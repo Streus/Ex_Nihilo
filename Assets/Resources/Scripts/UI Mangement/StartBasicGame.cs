dd5423f480ac2972fdc5ea0953ea196288ac250b
Streusiladei Malieavek <embersteelstreus@gmail.com>
2016-07-31 01:14:52 -0500

    Front End mostly completed
    Back end and most of front end in a workable state

---
+++ Assets/Resources/Scripts/UI Mangement/StartBasicGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartBasicGame : MonoBehaviour 
+{
+	public void gotoGameScene()
+	{
+		SceneManager.LoadScene("Game");
+	}
+}
