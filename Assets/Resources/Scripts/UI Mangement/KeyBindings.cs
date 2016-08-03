f3ccc4afe0fdd1ed598c25347c04deff446e6941
Streusiladei Malieavek <embersteelstreus@gmail.com>
2016-08-03 17:32:17 -0500

    Added DevConsole
    Added an in-game console for doing fun stuff.

---
+++ Assets/Resources/Scripts/UI Mangement/KeyBindings.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public class KeyBindings : MonoBehaviour {
+
+	//movement set 1
+	static KeyCode forward;
+	static KeyCode backward;
+	static KeyCode turnRight;
+	static KeyCode turnLeft;
+
+	//movement set 2
+	static KeyCode placeMvtMkr;
+
+	//abilities
+	static KeyCode ability1;
+	static KeyCode ability2;
+	static KeyCode ability3;
+	static KeyCode ability4;
+	static KeyCode ability5;
+	static KeyCode ability6;
+
+	//misc
+	static KeyCode interact;
+
+	// Use this for initialization
+	void Start() 
+	{
+		loadKeyBindings ();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+
+	// Fill the KeyCode fields with values from keybindings.ini
+	public void loadKeyBindings() 
+	{
+		
+	}
+}
