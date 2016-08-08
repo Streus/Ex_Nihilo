c9324a6d9ef72e9bef8433eff393f08312268276
Streusiladei Malieavek <embersteelstreus@gmail.com>
2016-08-08 03:11:27 -0500

    Minor changes.

--- Assets/Resources/Scripts/UI Mangement/KeyRebindButton.cs
+++ Assets/Resources/Scripts/UI Mangement/KeyRebindButton.cs
@@ -4,12 +4,51 @@ using System.Collections;
 public class KeyRebindButton : MonoBehaviour {
 
 	public string key;
+	private KeyCode* keyVar;
 
 	bool rebinding = false;
 
 	// Use this for initialization
 	void Start () {
 		key = "default";
+		switch (key) {
+		case "forward":
+			keyVar = KeyBindings.forward;
+			break;
+		case "backward":
+			keyVar = KeyBindings.backward;
+			break;
+		case "turnRight":
+			keyVar = KeyBindings.turnRight;
+			break;
+		case "turnLeft":
+			keyVar = KeyBindings.turnLeft;
+			break;
+		case "placeMvtMkr":
+			keyVar = KeyBindings.placeMvtMkr;
+			break;
+		case "ability1":
+			keyVar = KeyBindings.ability1;
+			break;
+		case "ability2":
+			keyVar = KeyBindings.ability2;
+			break;
+		case "ability3":
+			keyVar = KeyBindings.ability3;
+			break;
+		case "ability4":
+			keyVar = KeyBindings.ability4;
+			break;
+		case "ability5":
+			keyVar = KeyBindings.ability5;
+			break;
+		case "ability6":
+			keyVar = KeyBindings.ability6;
+			break;
+		case "interact":
+			keyVar = KeyBindings.interact;
+			break;
+		}
 	}
 
 	// Update is called once per frame
@@ -21,44 +60,7 @@ public class KeyRebindButton : MonoBehaviour {
 				if(Input.GetKey(vKey))
 					newKey = vKey;
 			}
-			switch (key) {
-			case "forward":
-				KeyBindings.forward = newKey;
-				break;
-			case "backward":
-				KeyBindings.backward = newKey;
-				break;
-			case "turnRight":
-				KeyBindings.turnRight = newKey;
-				break;
-			case "turnLeft":
-				KeyBindings.turnLeft = newKey;
-				break;
-			case "placeMvtMkr":
-				KeyBindings.placeMvtMkr = newKey;
-				break;
-			case "ability1":
-				KeyBindings.ability1 = newKey;
-				break;
-			case "ability2":
-				KeyBindings.ability2 = newKey;
-				break;
-			case "ability3":
-				KeyBindings.ability3 = newKey;
-				break;
-			case "ability4":
-				KeyBindings.ability4 = newKey;
-				break;
-			case "ability5":
-				KeyBindings.ability5 = newKey;
-				break;
-			case "ability6":
-				KeyBindings.ability6 = newKey;
-				break;
-			case "interact":
-				KeyBindings.interact = newKey;
-				break;
-			}
+			*keyVar = newKey;
 			rebinding = false;
 		}
 	}
