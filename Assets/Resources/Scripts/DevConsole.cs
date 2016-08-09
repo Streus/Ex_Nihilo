5f305a6abf68d380fbd78981a896dc20d75257a9
rushingseas8 <georgealeks@hotmail.com>
2016-08-09 00:07:10 -0500

    Unborked the dev console removal

--- Assets/Resources/Scripts/DevConsole.cs
+++ Assets/Resources/Scripts/DevConsole.cs
@@ -18,13 +18,10 @@ public class DevConsole : MonoBehaviour {
 	private static HistoryBuffer cmdHistory;
 	public int historySize;
 	public int hisIndex;
-<<<<<<< HEAD
 
 	//Command related variables
 	public static Assembly assembly = Assembly.GetExecutingAssembly ();
 	public static ArrayList commands = new ArrayList ();
-=======
->>>>>>> origin/staging
 
 	// Use this for initialization
 	void Start () {
