a066eb075fa973f825ff8c69b4f2423ebb8d11ee
rushingseas8 <georgealeks@hotmail.com>
2016-08-12 03:20:35 -0500

    Cell Movement Style 2 Improvements
    *Modified the algorithm slightly to increase performance
    -Removed exponential movement speed (-> now fastest possible)
    -Removed slower turn speed (-> now fastest possible)
    +Added flag

---
+++ Assets/Resources/Scripts/AI/NavigateAI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigateAI : BaseAI {
+
+	private float targetX, targetY;
+
+	// Use this for initialization
+	public override void Start () {
+		targetX = 0;
+		targetY = 0;
+
+		cell.turn (Random.Range (0f, Mathf.PI * 2));
+
+		cell.maxSpeed *= 0.4f;
+		//cell.turnSpeed *= 0.4f;
+	}
+
+	// Update is called once per frame
+	public override void Update () {
+		if (cell.navigateTo (targetX, targetY)) {
+			targetX = Random.Range (-10f, 10f) + cell.position.x;
+			targetY = Random.Range (-10f, 10f) + cell.position.y;
+		}
+	}
+
+	public override string toString() {
+		return "Navigate AI";
+	}
+}
