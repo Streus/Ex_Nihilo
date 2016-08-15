3fec3218c182df2bc9053a5a717c9257fc60a8ae
Streusiladei Malieavek <embersteelstreus@gmail.com>
2016-08-14 23:40:07 -0500

    Started work on In-Game GUI

--- Assets/Resources/Scripts/UI Mangement/PanBorderSizeSlider.cs
+++ Assets/Resources/Scripts/UI Mangement/PanBorderSizeSlider.cs
@@ -10,7 +10,6 @@ public class PanBorderSizeSlider : MonoBehaviour {
 	void Start(){
 		borderSlider = transform.GetChild (1).GetComponent<Slider> ();
 		borderSlider.value = GameOptions.panBorderSize;
-		borderSlider.maxValue = Mathf.Min(Screen.width, Screen.height)/2;
 
 		textElement = transform.GetChild(0).GetComponent<Text>();
 		textElement.text = "Pan Border Size: " + borderSlider.value;
@@ -22,7 +21,6 @@ public class PanBorderSizeSlider : MonoBehaviour {
 	}
 
 	public void Update(){
-		borderSlider.maxValue = Mathf.Min(Screen.width, Screen.height)/2;
 		textElement.text = "Pan Border Size: " + borderSlider.value;
 	}
 }
