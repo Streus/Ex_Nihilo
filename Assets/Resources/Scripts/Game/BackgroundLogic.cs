136a34dca9bf79eb3ea3d005d01e4ef85bdcbb58
rushingseas8 <georgealeks@hotmail.com>
2016-08-18 18:32:07 -0500

    bg test

--- Assets/Resources/Scripts/Game/BackgroundLogic.cs
+++ Assets/Resources/Scripts/Game/BackgroundLogic.cs
@@ -2,12 +2,11 @@
 using System.Collections;
 
 public class BackgroundLogic : MonoBehaviour {
-
-	//Height of the quad- should be behind the camera (< -10)
-	private static int quadHeight = -20;
-
 	//Height of the background image- should be far in BG (>> 0)
 	private static int bgHeight = 100;
+
+	//Rotation of the plane - needed for proper instantiation
+	private static Quaternion rotation = Quaternion.Euler(0, -90, 90);
 
 	/**
 	 * These are the center coordinates in "background tile units"- 
@@ -16,55 +15,89 @@ public class BackgroundLogic : MonoBehaviour {
 	 */
 	public static float cenX, cenY;
 
-	/**
-	 * How large is the background? The background array will be of
-	 * size NxN, where N = (DIM * 2) + 1. This ensures that the
-	 * array is always odd. 
-	 * 
-	 * If this is larger, you can zoom out further without losing the 
-	 * appearance of a constant background.
-	 */
-	public static int DIM = 12;
-	private static int N;
+	public static GameObject bg;
+	private static Texture2D tex;
 
-	/**
-	 * An array of background tile objects
-	 */
-	public static GameObject[,] bgArr;
+	private static int textureRes = 256;
+
+	public static Gradient gradient = new Gradient();
 
 	// Use this for initialization
 	void Start () {
 		cenX = 0;
 		cenY = 0;
 
-		setDim (3);
+		/*
+		GradientColorKey[] gck = new GradientColorKey[2];
+		gck [0].color = new Color (0, 0, 0);
+		gck [0].time = 0;
+		gck [1].color = new Color (0, 255, 255);
+		gck [1].time = 1;
+		gradient.colorKeys = gck;
+		*/
+
+		Vector3 imagePos = new Vector3 (10 * cenX, 10 * cenY, bgHeight);
+		bg = Game.create ("Background", imagePos, rotation);
+
+		generate (1);
 	}
 
-	public static void setDim(int to) {
-		if (to < 1)
-			return;
+	private static void generate(int steps) {
+		tex = new Texture2D (textureRes, textureRes);
+		tex.wrapMode = TextureWrapMode.Clamp;
+		//tex.filterMode = FilterMode.Point;
 
-		N = (2 * DIM) + 1;
-		bgArr = new GameObject[N, N];
+		//start at 1x and go up by 2 per step
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				//quadHeight is behind the camera, and so invisible.
-				//bgHeight is far into the distance, and so is rendered in the background.
-				Vector3 quadPos = new Vector3 (10 * (cenX + i - DIM), 10 * (cenY + j - DIM), quadHeight);
-				Vector3 imagePos = new Vector3 (10 * (cenX + i - DIM), 10 * (cenY + j - DIM), bgHeight);
+		Color[] colorArr = new Color[textureRes * textureRes];
 
-				bgArr[i, j] = (GameObject)Instantiate(
-					Resources.Load("Prefabs/Background"),
-					quadPos,
-					Quaternion.identity);
-				bgArr [i, j].transform.GetChild (0).position = imagePos;
+		float[] values = new float[textureRes * textureRes];
+
+		float correction = 0;
+		float freq = 4f;
+		float ampl = 2f;
+		float scale = freq / textureRes;
+
+		float offsetX = (Random.value * 2000) - 1000;
+		float offsetY = (Random.value * 2000) - 1000;
+		for (int s = 0; s < steps; s++) {
+			freq *= 2;
+			ampl *= 0.5f;
+			scale *= 0.5f;
+			correction += ampl;
+
+			for (int i = 0; i < textureRes; i++) {
+				for (int j = 0; j < textureRes; j++) {
+					float val = ampl * Mathf.PerlinNoise (
+						offsetX + (scale * i),
+						offsetY + (scale * j));
+					//colorArr [(i * textureRes) + j] = sum(
+					//	new Color (val, val, val), colorArr[(i * textureRes) + j]);
+					values[(i * textureRes) + j] += val;
+				}
 			}
 		}
+
+		correction = 1 / correction;
+		
+		for (int i = 0; i < textureRes * textureRes; i++) {
+			values [i] *= correction;
+			colorArr [i] = new Color (values [i], values [i], values [i]);
+		}
+
+		tex.SetPixels (colorArr);
+		tex.Apply ();
+
+		bg.GetComponent<Renderer> ().material.mainTexture = tex;
+	}
+
+	private static Color sum(Color a, Color b) {
+		return new Color ((a.r + b.r), (a.g + b.g), (a.b + b.b));
 	}
 
 	// Update is called once per frame
 	void Update () {
+		/*
 		//get main camera's position
 		Vector3 pos = Camera.main.transform.position;
 
@@ -83,14 +116,10 @@ public class BackgroundLogic : MonoBehaviour {
 
 			//create new array elements
 			for (int i = 0; i < N; i++) {
-				Vector3 quadPos = new Vector3 (10 * (cenX + DIM), 10 * (cenY + i - DIM), quadHeight);
 				Vector3 imagePos = new Vector3 (10 * (cenX + DIM), 10 * (cenY + i - DIM), bgHeight);
 
-				bgArr [N - 1, i] = (GameObject)Instantiate (
-					Resources.Load ("Prefabs/Background"),
-					quadPos,
-					Quaternion.identity);
-				bgArr [N - 1, i].transform.GetChild (0).position = imagePos;
+				bgArr [N - 1, i] = Game.create ("Background", imagePos, rotation);
+				bgArr [N - 1, i].GetComponent<Renderer> ().material.mainTexture = tex;
 			}
 		}
 
@@ -111,14 +140,10 @@ public class BackgroundLogic : MonoBehaviour {
 
 			//create new array elements
 			for (int i = 0; i < N; i++) {
-				Vector3 quadPos = new Vector3 (10 * (cenX - DIM), 10 * (cenY + i - DIM), quadHeight);
 				Vector3 imagePos = new Vector3 (10 * (cenX - DIM), 10 * (cenY + i - DIM), bgHeight);
 
-				bgArr [0, i] = (GameObject)Instantiate (
-					Resources.Load ("Prefabs/Background"),
-					quadPos,
-					Quaternion.identity);
-				bgArr [0, i].transform.GetChild (0).position = imagePos;
+				bgArr [0, i] = Game.create ("Background", imagePos, rotation);
+				bgArr [0, i].GetComponent<Renderer> ().material.mainTexture = tex;
 			}
 		}
 
@@ -134,14 +159,10 @@ public class BackgroundLogic : MonoBehaviour {
 			cenY++;
 
 			for (int i = 0; i < N; i++) {
-				Vector3 quadPos = new Vector3 (10 * (cenX + i - DIM), 10 * (cenY + DIM), quadHeight);
 				Vector3 imagePos = new Vector3 (10 * (cenX + i - DIM), 10 * (cenY + DIM), bgHeight);
 
-				bgArr [i, N - 1] = (GameObject)Instantiate (
-					Resources.Load("Prefabs/Background"),
-					quadPos,
-					Quaternion.identity);
-				bgArr [i, N - 1].transform.GetChild (0).position = imagePos;
+				bgArr [i, N - 1] = Game.create ("Background", imagePos, rotation);
+				bgArr [i, N - 1].GetComponent<Renderer> ().material.mainTexture = tex;
 			}
 		}
 
@@ -157,15 +178,12 @@ public class BackgroundLogic : MonoBehaviour {
 			cenY--;
 
 			for (int i = 0; i < N; i++) {
-				Vector3 quadPos = new Vector3 (10 * (cenX + i - DIM), 10 * (cenY - DIM), quadHeight);
 				Vector3 imagePos = new Vector3 (10 * (cenX + i - DIM), 10 * (cenY - DIM), bgHeight);
 
-				bgArr [i, 0] = (GameObject)Instantiate (
-					Resources.Load ("Prefabs/Background"),
-					quadPos,
-					Quaternion.identity);
-				bgArr [i, 0].transform.GetChild (0).position = imagePos;
+				bgArr [i, 0] = Game.create ("Background", imagePos, rotation);
+				bgArr [i, 0].GetComponent<Renderer> ().material.mainTexture = tex;
 			}
 		}
+		*/
 	}
 }
